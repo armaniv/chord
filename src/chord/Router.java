e878b3ffe8ee70101d20cd37c24c35ce8cc0c9e8
Marian Diaconu <neboduus@gmail.com>
2019-12-10 12:18:12 +0100

    adds delay to message propagation

--- src/chord/Router.java
+++ src/chord/Router.java
@@ -7,6 +7,7 @@ import repast.simphony.engine.environment.RunEnvironment;
 import repast.simphony.engine.schedule.ISchedule;
 import repast.simphony.engine.schedule.PriorityType;
 import repast.simphony.engine.schedule.ScheduleParameters;
+import repast.simphony.random.RandomHelper;
 
 public class Router {
 	private HashMap<Integer, Node> nodes; 
@@ -26,11 +27,15 @@ public class Router {
 		
 		ISchedule schedule = RunEnvironment.getInstance().getCurrentSchedule();
 		ScheduleParameters scheduleParameters = 
-				ScheduleParameters.createOneTime(schedule.getTickCount() + 1, PriorityType.RANDOM);
+				ScheduleParameters.createOneTime(schedule.getTickCount() + randomDelay(), PriorityType.RANDOM);
 		schedule.schedule(scheduleParameters, new ReceiveMessage(nodes.get(message.getDestinationNode()), message));
 	}
 	
 	public Boolean isUp(Integer nodeId) {
 		return !nodes.get(nodeId).isCrashed();
+	}
+	
+	public int randomDelay() {
+		return RandomHelper.nextIntFromTo(1, 2);
 	}
 }
